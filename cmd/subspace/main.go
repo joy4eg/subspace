fa8a3f3a8dfbe385381f2bf5935fa4610a1f31b7
Yaroslav Syrytsia <me@ys.lc>
2024-06-20 12:39:07 +0200

    go: upgrade mmodules

--- cmd/subspace/main.go
+++ cmd/subspace/main.go
@@ -390,9 +390,6 @@ func configureSAML() error {
 		Certificate:       keyPair.Leaf,
 		IDPMetadata:       entity,
 		CookieName:        SessionCookieNameSSO,
-		CookieDomain:      httpHost, // TODO: this will break if using a custom domain.
-		CookieSecure:      !httpInsecure,
-		Logger:            logger,
 		AllowIDPInitiated: true,
 	})
 	if err != nil {
